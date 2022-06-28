0f44a098711d7cd14635e4a8d80e3d5242655378
Douglas Silva <silvatec.br@gmail.com>
2022-06-27 23:29:27 -0300

    instalando dependencias pelo composer

---
+++ buscar-cursos.php
@@ -0,0 +1,9 @@
+<?php
+
+use GuzzleHttp\Client;
+
+$client = new Client();
+
+$resposta = $client->request('GET', 'https://cursos.alura.com.br/formacao-avancando-php');
+
+$html = $resposta->getBody();
