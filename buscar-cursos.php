fde765ab417803034830ecbcc5644cde1565ff90
Douglas Silva <silvatec.br@gmail.com>
2022-06-28 15:21:07 -0300

    criando classe buscador

--- buscar-cursos.php
+++ buscar-cursos.php
@@ -1,9 +1,18 @@
 <?php
 
+require 'vendor/autoload.php';
+require 'src/Buscador.php';
+
+use Alura\BuscadorDeCursos\Buscador;
 use GuzzleHttp\Client;
+use Symfony\Component\DomCrawler\Crawler;
 
-$client = new Client();
+$client = new Client(['base_uri' => 'https://www.alura.com.br/']);
+$crawler = new Crawler();
 
-$resposta = $client->request('GET', 'https://cursos.alura.com.br/formacao-avancando-php');
+$buscador = new Buscador($client, $crawler);
+$cursos = $buscador->buscar('/cursos-online-programacao/php');
 
-$html = $resposta->getBody();
+foreach ($cursos as $curso) {
+    echo $curso . PHP_EOL;
+}
